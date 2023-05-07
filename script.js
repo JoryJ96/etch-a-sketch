c1bdd7af08aca051acecc6bd282f62538d372d94
Joey Jacobson <joeyj1996@proton.me>
2023-05-06 20:48:09 -0500

    Window shows 16x16 grid of appropriately sized divs, and reconfigures when window is resized

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-boardSetup();
+
+
+// size determination on initial load
+window.addEventListener('load', function() {
+    size();
+});
 
 // Re-runs size determination when window is resized
 window.addEventListener('resize', function() {
     size();
 })
 
-function boardSetup() {
-    // Using nested for loop, generate empty square divs, 16x16
-    const board = document.querySelector('.cell-container');
-    board.innerHTML = '';
-
-    window.addEventListener('load', function() {
-        size();
-    });
-}
-
 // Generates elements based on window size. Sizing/layout are handled by CSS
 function size() {
-    // Floors determine the width / height of the divs
-    var widthFloor = Math.floor(window.innerWidth / (1 / 16));
-    var heightFloor = Math.floor(window.innerHeight / (1 / 16));
-
-    for (let i = 0; i < widthFloor * heightFloor; i++) {
+    const board = document.querySelector('.cell-container');
+    board.innerHTML = '';
+    
+    // console.log("size() is running")
+    for (let i = 0; i < 256; i++) {
         var square = document.createElement('div');
         square.className = 'square';
         board.appendChild(square);
